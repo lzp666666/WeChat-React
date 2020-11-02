8022a3ef8ddb2e8818fa6d10ca969afbe44ca4d2
赖泽平 <1287096778@qq.com>
2020-11-02 15:00:43 +0800

    引入组件？

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './index.scss'
-import { NavBar, Icon, Flex } from 'antd-mobile';
+import { NavBar, Icon, Flex, Modal } from 'antd-mobile';
+import rightPop from '../../component/rightPop';
 class index extends Component {
     constructor(props) {
         super(props)
@@ -40,6 +41,19 @@ class index extends Component {
                         )
                     })
                 }
+                <Modal
+                    visible={true}
+                    transparent
+                    maskClosable={true}
+                >
+                    <div style={{ height: '100%' }}>
+                        <div>
+                        <Icon type={'check-circle'}></Icon>
+                        <span>添加好友</span>
+                        <rightPop />
+                        </div>
+                    </div>
+                </Modal>
             </div>
         )
     }
